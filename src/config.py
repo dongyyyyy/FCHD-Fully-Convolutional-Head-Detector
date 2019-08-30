18c146a6e22ca0cf5c73c96dd06875a6ba740673
dongyyyyy <dongyoung0218@gmail.com>
2019-08-30 10:46:41 +0900

    update

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ class Config:
     hollywood_dataset_root_path = '/home/aditya-tyco/Desktop/aditya_personal_projects/head_detection_v5/data/HollywoodHeads'
     min_size = 600  # image resize
     max_size = 1000 # image resize 
-    caffe_pretrain = True
-    caffe_pretrain_path = '/home/aditya-tyco/Desktop/aditya_personal_projects/head_detection_v5/data/pretrained_model/vgg16_caffe.pth'
+    #caffe_pretrain = True
+    #caffe_pretrain_path = '/home/aditya-tyco/Desktop/aditya_personal_projects/head_detection_v5/data/pretrained_model/vgg16_caffe.pth'
     model_save_path = '/home/aditya-tyco/Desktop/aditya_personal_projects/head_detection_v5/checkpoints'
     # sigma for l1_smooth_loss
     rpn_sigma = 3.
